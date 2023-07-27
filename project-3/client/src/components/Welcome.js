b68b6ac1c78011d16fb5728a02fbff9ba13f8c8e
[Dan <123705636+dkopac@users.noreply.github.com>
2023-07-26 20:25:24 -0400

    set route to background image for welcome page

---
+++ project-3/client/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = ({setCurrentPage}) => {
+  return (
+    <div style={{ 
+        backgroundImage: `../../assets/OPEN-MIC.webp`
+      }}>
+        </div>
+  )
+}
+
+export default Welcome
