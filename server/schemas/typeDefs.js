f51a8051276fc8ece33ac053a4385522a761c353
paulisturm <paulisturm22@gmail.com>
2023-07-31 20:53:00 -0400

    type defs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    favoriteMics: [Mic]!
+  }
+  type Mic {
+    micId: ID
+    venue: String
+    description: String
+    host: String
+    covercharge: String 
+}
+type Auth {
+    token: String
+    user: User
+}
+type Query {
+    
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+  `
