2799df06204e2e966a3a4675d50a40040577a9ff
[Dan <123705636+dkopac@users.noreply.github.com>
2023-07-31 20:58:52 -0400

    project-3 on Heroku

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+const styles = {
+    footer: {
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        width: "100%",
+        backgroundColor: "none",
+        color: "white",
+        textAlign: "right",
+        padding: "10px"
+    }
+}
+
 export default function Footer() {
     return (
-        <footer className="justify-content-center fixed-bottom">
-        <div className="container">
-            <span className="text-muted">Made By Paul Sturm, Seth Andregg, Jacob Andrew, and Dan Kopac.</span>
-        </div>
+        <footer style={styles.footer}>
+            <p>Created by: <a href="">Paul Sturm, Seth Andregg, Jacob Andrew, and Dan Kopac.</a></p>
         </footer>
     );
     }
