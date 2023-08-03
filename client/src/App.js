2c34572f5c19bf8ac6b538b301720ec02527ad3e
[Dan <123705636+dkopac@users.noreply.github.com>
2023-08-02 20:36:25 -0400

    favorites added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Mics from './components/Mics';
 import Login from './components/Login';
 import Favorites from './components/Favorites';
 import Welcome from './components/Welcome'
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Image from './assets/Mic.webp'
 
 function App() {
--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Favorites () {
     return (
-        <></>
+        <div>
+            <h1>Favorites</h1>
+        </div>
     )
 }
 
-export default Favorites;
+export default Favorites
