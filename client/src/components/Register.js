c17f9c2cba6fdea6710be29c7aab28720e32b6f7
paulisturm <paulisturm22@gmail.com>
2023-08-02 21:02:58 -0400

    register

---
+++ client/src/components/Register.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Register = () => {
+  // set initial form state
+  const [ username, setUsername] = useState('');
+  const [ email, setEmail] = useState('');
+  const [ password, setPassword] = useState('');
+ 
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+console.log(error)
+ 
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+    
+    try {
+      const { data } = await addUser({
+        variables: { email, username, password },
+        
+      });
+      console.log(data);
+     
+      setEmail("")
+      setPassword("")
+      setUsername("")
+
+      console.log("tryblock", data);
+      Auth.login(data.addUser.token);
+    } catch (err) {
+      
+      console.error("something", err);
+    };
+    
+
+  };
+
+  return (
+    <div className="register">
+      {/* This is needed for the validation functionality above */}
+      <Form onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your signup!
+        </Alert>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='username'>Username</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your username'
+            name='username'
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!username || !email || !password}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
