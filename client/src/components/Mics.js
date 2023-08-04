72b6c695b985939f70bf9f39de869efe2c66b0f9
[Dan <123705636+dkopac@users.noreply.github.com>
2023-08-04 10:00:03 -0400

    added links for venues in the area

--- client/src/components/Mics.js
+++ client/src/components/Mics.js
@@ -1,105 +1,27 @@
-import React from 'react'
+import React from 'react';
+import BMC from '../assets/BMC.jpg';
+import Agora from '../assets/The Agora.jpg';
+import MusicBox from '../assets/Music Box.jpg';
+import Playhouse from '../assets/Playhouse Square.jpg';
+import HOB from '../assets/HOB-Cleveland.jpg'
+import JP from '../assets/Jacobs-Pavillion.jpg'
 
-const Styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '90vh',
-        width: '100vw',
-        backgroundColor: 'black',
-        color: 'white',
-        fontSize: '2rem',
-        fontFamily: 'monospace',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        padding: '1rem',
-        margin: '1rem',
-        border: '1px solid white',
-        borderRadius: '1rem',
-        boxShadow: '0 0 10px white',
-        textShadow: '0 0 10px white',
-    },
-    image: {
-        height: '50vh',
-        width: '50vw',
-        border: '1px solid white',
-        borderRadius: '1rem',
-        boxShadow: '0 0 10px white',
-        textShadow: '0 0 10px white',
-    }
-}
 
-function Mics (props) {
+function Mics() {
     return (
-        <div className="container" style={Styles.container}>
-            <div className="img-container" style={Styles.image}>
-                <img src={props.image} alt={props.name} />
+        <><h1>Mics</h1>
+        <p className="text-center">Checkout the upcoming events at these venues</p>
+        <div className="container">
+            <a className='col' href='https://www.clevelandamphitheater.com'> <img style={{ width:300, height: 200}} src= {BMC}></img></a>
+            <a className='col' href='https://www.agoracleveland.com/'> <img style={{ width:300, height: 200}} src= {Agora}></img></a>
+            <a className='col' href='https://musicboxcle.com'> <img style={{ width:300, height: 200}} src= {MusicBox}></img></a>
+            <a className='col' href='https://www.playhousesquare.org'> <img style={{ width:300, height: 200}} src= {Playhouse}></img></a>
+            <a className='col' href='https://www.houseofblues.com/cleveland'> <img style={{ width:300, height: 200}} src= {HOB}></img></a>
+            <a className='col' href='https://www.jacobspavilion.com/'> <img style={{ width:300, height: 200}} src= {JP}></img></a>
             </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        <strong>Location:</strong> {props.location}
-                    </li>
-                    <li>
-                        <strong>Host:</strong> {props.host}
-                    </li>
-                    <li>
-                        <strong>Date/Time:</strong> {props.date}
-                    </li>
-                    <li>
-                        <strong>Description:</strong> {props.description}
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+        </>
+
+    )
 }
 
-export default function MicsList() {
-    const mics = [
-        {
-            Location: 'Blossom Music Center',
-            Host: 'Live Nation',
-            Date: 'August 11, 2023',
-            Description: 'The Black Crowes Present: Shake Your Money Maker',
-            image: '../assets/BMC.jpg'
-        },
-        {
-            Location: 'Playhouse Square',
-            Host: 'Playhouse Square',
-            Date: 'August 18, 2023',
-            Description: 'Hamilton',
-            image: '../assets/Playhouse Square.jpg'
-        },
-        {
-            Location: 'The Music Box Supper Club',
-            Host: 'The Music Box Supper Club',
-            Date: 'August 25, 2023',
-            Description: 'Queen Tribute: Queen Nation',
-            image: '../assets/Music Box.jpg'
-        },
-        {
-            Location: 'Geauga County Fairgrounds',
-            Host: 'Geauga County Fairgrounds',
-            Date: 'September 1, 2023',
-            Description: 'Cleveland Country Music Fest',
-            image: '../assets/Geauga County Fairgrounds.jpg'
-        },
-    ];
-
-    return (
-        <div>
-            {mics.map((mic) => (
-                <Mics
-                    location={mic.Location}
-                    host={mic.Host}
-                    date={mic.Date}
-                    description={mic.Description}
-                    image={mic.image}
-                />
-            ))}
-        </div>
-    );
-}
+export default Mics
