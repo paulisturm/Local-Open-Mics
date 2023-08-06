63db8adf812e6d4e77fc794f7e3d179145558b24
[Dan <123705636+dkopac@users.noreply.github.com>
2023-08-06 10:20:20 -0400

    Mics now lead to link to performers page at venue

--- client/src/components/Mics.js
+++ client/src/components/Mics.js
@@ -12,12 +12,12 @@ function Mics() {
             <h1>Mics</h1>
             <p className="text-center">Checkout the upcoming events at these venues</p>
             <div className="container">
-                <a className='col' href='https://www.clevelandamphitheater.com'> <img style={{ width: 300, height: 200 }} src={bonJovi}></img></a>
-                <a className='col' href='https://www.agoracleveland.com/'> <img style={{ width: 300, height: 200 }} src={jasonAldean}></img></a>
-                <a className='col' href='https://musicboxcle.com'> <img style={{ width: 300, height: 200 }} src={jimGaffigan}></img></a>
-                <a className='col' href='https://www.playhousesquare.org'> <img style={{ width: 300, height: 200 }} src={Stones}></img></a>
-                <a className='col' href='https://www.houseofblues.com/cleveland'> <img style={{ width: 300, height: 200 }} src={beachBoys}></img></a>
-                <a className='col' href='https://www.agoracleveland.com/events/detail/494709'> <img style= {{ width: 300, height: 200}} src={karaoke}></img> </a>
+                <a className='col' href='https://www.clevelandamphitheater.com/events/jason-aldean-mitchell-tenpenny-corey-kent-dee-jay-silver-17-september-2023/'> <img style={{ width: 300, height: 200 }} src={jasonAldean}></img></a>
+                <a className='col' href='https://www.agoracleveland.com/events/detail/494709'> <img style={{ width: 300, height: 200 }} src={karaoke}></img></a>
+                <a className='col' href='https://musicboxcle.com/event/stones-brunch-sep3/'> <img style={{ width: 300, height: 200 }} src={Stones}></img></a>
+                <a className='col' href='https://www.playhousesquare.org/events/detail/jim-gaffigan-barely-alive-tour'> <img style={{ width: 300, height: 200 }} src={jimGaffigan}></img></a>
+                <a className='col' href='https://www.houseofblues.com/cleveland/EventDetail?tmeventid=vvG1fZ9R67xf75&offerid=0'> <img style={{ width: 300, height: 200 }} src={bonJovi}></img></a>
+                <a className='col' href='https://www.jacobspavilion.com/events/detail/?event_id=479065'> <img style= {{ width: 300, height: 200}} src={beachBoys}></img> </a>
             </div>
         </>
     )
