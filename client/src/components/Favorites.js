22287f694015446aa3ac37ce315b4287bf4cd5b3
[Dan <123705636+dkopac@users.noreply.github.com>
2023-08-06 10:10:39 -0400

    Mics and Favorites added

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import BMC from '../assets/BMC.jpg';
+import Agora from '../assets/The Agora.jpg';
+import MusicBox from '../assets/Music Box.jpg';
+import Playhouse from '../assets/Playhouse Square.jpg';
+import HOB from '../assets/HOB-Cleveland.jpg'
+import JP from '../assets/Jacobs-Pavillion.jpg'
+
 
 function Favorites () {
     return (
-        <></>
+        <><h1>Favorites</h1>
+        <p className="text-center">Checkout the upcoming events at these venues</p>
+        <div className="container">
+            <a className='col' href='https://www.clevelandamphitheater.com'> <img style={{ width:300, height: 200}} src= {BMC}></img></a>
+            <a className='col' href='https://www.agoracleveland.com/'> <img style={{ width:300, height: 200}} src= {Agora}></img></a>
+            <a className='col' href='https://musicboxcle.com'> <img style={{ width:300, height: 200}} src= {MusicBox}></img></a>
+            <a className='col' href='https://www.playhousesquare.org'> <img style={{ width:300, height: 200}} src= {Playhouse}></img></a>
+            <a className='col' href='https://www.houseofblues.com/cleveland'> <img style={{ width:300, height: 200}} src= {HOB}></img></a>
+            <a className='col' href='https://www.jacobspavilion.com/'> <img style={{ width:300, height: 200}} src= {JP}></img></a>
+            </div>
+        </>
     )
 }
 
--- client/src/components/Mics.js
+++ client/src/components/Mics.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import BMC from '../assets/BMC.jpg';
-import Agora from '../assets/The Agora.jpg';
-import MusicBox from '../assets/Music Box.jpg';
-import Playhouse from '../assets/Playhouse Square.jpg';
-import HOB from '../assets/HOB-Cleveland.jpg'
-import JP from '../assets/Jacobs-Pavillion.jpg'
-
+import bonJovi from '../assets/Bon Jovi Tribute.jpg';
+import jasonAldean from '../assets/Jason Aldean 2023.jpg';
+import jimGaffigan from '../assets/Jim-Gaffigan.jpg';
+import Stones from '../assets/stones tribute.jpg';
+import beachBoys from '../assets/The Beach Boys.jpg';
+import karaoke from '../assets/Karaoke at the agora.jpg';
 
 function Mics() {
     return (
-        <><h1>Mics</h1>
-        <p className="text-center">Checkout the upcoming events at these venues</p>
-        <div className="container">
-            <a className='col' href='https://www.clevelandamphitheater.com'> <img style={{ width:300, height: 200}} src= {BMC}></img></a>
-            <a className='col' href='https://www.agoracleveland.com/'> <img style={{ width:300, height: 200}} src= {Agora}></img></a>
-            <a className='col' href='https://musicboxcle.com'> <img style={{ width:300, height: 200}} src= {MusicBox}></img></a>
-            <a className='col' href='https://www.playhousesquare.org'> <img style={{ width:300, height: 200}} src= {Playhouse}></img></a>
-            <a className='col' href='https://www.houseofblues.com/cleveland'> <img style={{ width:300, height: 200}} src= {HOB}></img></a>
-            <a className='col' href='https://www.jacobspavilion.com/'> <img style={{ width:300, height: 200}} src= {JP}></img></a>
+        <>
+            <h1>Mics</h1>
+            <p className="text-center">Checkout the upcoming events at these venues</p>
+            <div className="container">
+                <a className='col' href='https://www.clevelandamphitheater.com'> <img style={{ width: 300, height: 200 }} src={bonJovi}></img></a>
+                <a className='col' href='https://www.agoracleveland.com/'> <img style={{ width: 300, height: 200 }} src={jasonAldean}></img></a>
+                <a className='col' href='https://musicboxcle.com'> <img style={{ width: 300, height: 200 }} src={jimGaffigan}></img></a>
+                <a className='col' href='https://www.playhousesquare.org'> <img style={{ width: 300, height: 200 }} src={Stones}></img></a>
+                <a className='col' href='https://www.houseofblues.com/cleveland'> <img style={{ width: 300, height: 200 }} src={beachBoys}></img></a>
+                <a className='col' href='https://www.agoracleveland.com/events/detail/494709'> <img style= {{ width: 300, height: 200}} src={karaoke}></img> </a>
             </div>
         </>
-
     )
 }
 
